e84f73a7b5b782ac8d5882d83289c64a4d510d4c
marcoalonso <marcoalonsorota@hotmail.com>
2022-02-25 09:08:23 -0600

    Empezando

---
+++ CalculadoraIMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculadoraIMC
+//
+//  Created by marco rodriguez on 25/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
